e12b1585c836f5a44cbe8b2d627ea8e9a31985d2
Hirohisa Kawasaki <hirohisa.kawasaki@gmail.com>
2015-04-18 19:51:58 +0900

    Rename methods and clean code

--- PullToRefresh/CollectionViewController.swift
+++ PullToRefresh/CollectionViewController.swift
@@ -15,46 +15,46 @@ enum State {
 
 public class CollectionViewController: UICollectionViewController {
 
-    var refreshControl = UIRefreshControl()
+    public var refreshControl = UIRefreshControl()
     var state: State = .Ready
 
     public override func viewDidLoad() {
         super.viewDidLoad()
         collectionView?.addSubview(refreshControl)
-        refreshControl.addTarget(self, action: "viewWillRefresh", forControlEvents: UIControlEvents.ValueChanged)
+        refreshControl.addTarget(self, action: "_viewWillRefresh", forControlEvents: UIControlEvents.ValueChanged)
     }
 
     public func viewWillRefresh() {
-        let delay = 2.0 * Double(NSEC_PER_SEC)
-        let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-        dispatch_after(time, dispatch_get_main_queue(), {
-            self.viewDidRefresh()
-        })
     }
 
     public func viewDidRefresh() {
         refreshControl.endRefreshing()
     }
 
-    public func viewWillRequest() {
-        println("will")
-        state = .Reqesting
-        let delay = 2.0 * Double(NSEC_PER_SEC)
-        let time  = dispatch_time(DISPATCH_TIME_NOW, Int64(delay))
-        dispatch_after(time, dispatch_get_main_queue(), {
-            self.viewDidRequest()
-        })
+    public func viewWillLoadRequest() {
     }
 
-    public func viewDidRequest() {
+    public func viewDidLoadRequest() {
         state = .Ready
-        println("did")
     }
 
     public override func scrollViewDidScroll(scrollView: UIScrollView) {
         let bottom = scrollView.contentOffset.y + scrollView.frame.height
         if bottom >= scrollView.contentSize.height && state == .Ready {
-            viewWillRequest()
+            _viewWillLoadRequest()
         }
     }
+}
+
+extension CollectionViewController {
+
+    func _viewWillRefresh() {
+        self.viewWillRefresh()
+    }
+
+    func _viewWillLoadRequest() {
+        state = .Reqesting
+        self.viewWillLoadRequest()
+    }
+
 }
